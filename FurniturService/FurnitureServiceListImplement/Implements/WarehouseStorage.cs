e54ecfed868f945a3dcf23802e2bf78fad465c4c
RAFMAN <volkov.rafman@mail.ru>
2021-04-19 19:10:04 +0400

    Lab1_H_Update

--- FurniturService/FurnitureServiceListImplement/Implements/WarehouseStorage.cs
+++ FurniturService/FurnitureServiceListImplement/Implements/WarehouseStorage.cs
@@ -174,18 +174,5 @@ namespace FurnitureServiceListImplement.Implements
 
             CreateModel(model, tempWarehouse);
         }
-
-        public void Print()
-        {
-            foreach (Warehouse warehouse in source.Warehouses)
-            {
-                Console.WriteLine(warehouse.WarehouseName + " " + warehouse.FullNameOfTheHead + " " + warehouse.DateCreate);
-                foreach (KeyValuePair<int, int> keyValue in warehouse.WarehouseComponents)
-                {
-                    string componentName = source.Components.FirstOrDefault(component => component.Id == keyValue.Key).ComponentName;
-                    Console.WriteLine(componentName + " " + keyValue.Value);
-                }
-            }
-        }
     }
 }
