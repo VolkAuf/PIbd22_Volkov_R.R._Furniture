33c178952ee5eaa678b6df7bb88c155b29b101c7
RAFMAN <volkov.rafman@mail.ru>
2021-04-20 01:30:14 +0400

    Laba4_Update

--- FurniturService/FurnitureServiceListImplement/Implements/OrderStorage.cs
+++ FurniturService/FurnitureServiceListImplement/Implements/OrderStorage.cs
@@ -33,7 +33,7 @@ namespace FurnitureServiceListImplement.Implements
             List<OrderViewModel> result = new List<OrderViewModel>();
             foreach (var order in source.Orders)
             {
-                if (order.FurnitureId.ToString().Contains(model.FurnitureId.ToString()))
+                if (order.DateCreate >= model.DateFrom && order.DateCreate <= model.DateTo)
                 {
                     result.Add(CreateModel(order));
                 }
