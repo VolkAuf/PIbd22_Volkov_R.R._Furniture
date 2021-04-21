c5fdc2595d2c4819e13f11ad56e17d3087cc2a46
RAFMAN <volkov.rafman@mail.ru>
2021-04-21 11:25:40 +0400

    Laba4_H_WordWarehouseAdd

---
+++ FurniturService/FurniturServiceBusinessLogic/BusinessLogics/SaveWarehouseToWord.cs
@@ -0,0 +1,169 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using FurnitureServiceBusinessLogic.HelperModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FurnitureServiceBusinessLogic.BusinessLogics
+{
+    static class SaveWarehouseToWord
+    {
+        /// <summary>
+        /// Создание документа
+        /// </summary>
+        /// <param name="info"></param>
+        public static void CreateDoc(WarehouseWordInfo info)
+        {
+            using (WordprocessingDocument wordDocument = WordprocessingDocument.Create(info.FileName, WordprocessingDocumentType.Document))
+            {
+                MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+                mainPart.Document = new Document();
+                Body docBody = mainPart.Document.AppendChild(new Body());
+                docBody.AppendChild(CreateParagraph(new WordParagraph
+                {
+                    Texts = new List<(string, WordParagraphProperties)> { (info.Title, new WordParagraphProperties { Bold = true, Size = "24", }) },
+                    ParagraphProperties = new WordParagraphProperties
+                    {
+                        Size = "24",
+                        JustificationValues = JustificationValues.Center
+                    }
+                }));
+                Table table = new Table();
+
+                // Create a TableProperties object and specify its border information.
+                TableProperties tblProp = new TableProperties(
+                    new TableBorders(
+                        new TopBorder() { Val = new EnumValue<BorderValues>(BorderValues.Dashed), Size = 24 },
+                        new BottomBorder() { Val = new EnumValue<BorderValues>(BorderValues.Dashed), Size = 24 },
+                        new LeftBorder() { Val = new EnumValue<BorderValues>(BorderValues.Dashed), Size = 24 },
+                        new RightBorder() { Val = new EnumValue<BorderValues>(BorderValues.Dashed), Size = 24 },
+                        new InsideHorizontalBorder() { Val = new EnumValue<BorderValues>(BorderValues.Dashed), Size = 24 },
+                        new InsideVerticalBorder() { Val = new EnumValue<BorderValues>(BorderValues.Dashed), Size = 24 }
+                    )
+                );
+                // Append the TableProperties object to the empty table.
+                table.AppendChild<TableProperties>(tblProp);
+                TableRow tr = new TableRow();
+                TableCell tc1 = new TableCell();
+                
+                // Specify the width property of the table cell.
+                tc1.Append(new TableCellProperties(new TableCellWidth() { Type = TableWidthUnitValues.Dxa, Width = "2400" }));
+
+                // Specify the table cell content.
+                tc1.Append(new Paragraph(new Run(new Text("Название"))));
+                tr.Append(tc1);
+                tc1.Append(new Paragraph(new Run(new Text("ФИО"))));
+                tr.Append(tc1);
+                tc1.Append(new Paragraph(new Run(new Text("Дата создания"))));
+                tr.Append(tc1);
+                table.Append(tr);
+                foreach (var warehouse in info.Warehouses)
+                {
+                    docBody.AppendChild(CreateParagraph(new WordParagraph
+                    {
+                        Texts = new List<(string, WordParagraphProperties)> { (warehouse.WarehouseName + " : ", new WordParagraphProperties { Size = "24", Bold = false }), (warehouse.FullNameOfTheHead + " : " , new WordParagraphProperties { Size = "24", Bold = false }), (warehouse.DateCreate.ToString(), new WordParagraphProperties { Size = "24", Bold = false })},
+                        ParagraphProperties = new WordParagraphProperties
+                        {
+                            Size = "24",
+                            JustificationValues = JustificationValues.Both
+                        }
+                    }));
+
+                    TableRow temptr = new TableRow();
+
+                    TableCell temptc1 = new TableCell();
+                    temptc1.Append(new TableCellProperties(new TableCellWidth() { Type = TableWidthUnitValues.Dxa, Width = "2400" }));
+                    temptc1.Append(new Paragraph(new Run(new Text(warehouse.WarehouseName))));
+                    TableCell temptc2 = new TableCell();
+                    temptc2.Append(new TableCellProperties(new TableCellWidth() { Type = TableWidthUnitValues.Dxa, Width = "2400" }));
+                    temptc2.Append(new Paragraph(new Run(new Text(warehouse.FullNameOfTheHead))));
+                    TableCell temptc3 = new TableCell();
+                    temptc3.Append(new TableCellProperties(new TableCellWidth() { Type = TableWidthUnitValues.Dxa, Width = "2400" }));
+                    temptc3.Append(new Paragraph(new Run(new Text(warehouse.DateCreate.ToString()))));
+
+                    // Append the table cell to the table row.
+                    temptr.Append(temptc1);
+                    temptr.Append(temptc2);
+                    temptr.Append(temptc3);
+                    table.Append(temptr);
+                }
+                docBody.AppendChild(CreateSectionProperties());
+                docBody.AppendChild(table);
+                wordDocument.MainDocumentPart.Document.Save();
+            }
+        }
+        /// <summary>
+        /// Настройки страницы
+        /// </summary>
+        /// <returns></returns>
+        private static SectionProperties CreateSectionProperties()
+        {
+            SectionProperties properties = new SectionProperties();
+            PageSize pageSize = new PageSize
+            {
+                Orient = PageOrientationValues.Portrait
+            };
+            properties.AppendChild(pageSize);
+            return properties;
+        }
+        /// <summary>
+        /// Создание абзаца с текстом
+        /// </summary>
+        /// <param name="paragraph"></param>
+        /// <returns></returns>
+        private static Paragraph CreateParagraph(WordParagraph paragraph)
+        {
+            if (paragraph != null)
+            {
+                Paragraph docParagraph = new Paragraph();
+                docParagraph.AppendChild(CreateParagraphProperties(paragraph.ParagraphProperties));
+                foreach (var run in paragraph.Texts)
+                {
+                    Run docRun = new Run();
+                    RunProperties properties = new RunProperties();
+                    properties.AppendChild(new FontSize { Val = run.Item2.Size });
+                    if (run.Item2.Bold)
+                    {
+                        properties.AppendChild(new Bold());
+                    }
+                    docRun.AppendChild(properties);
+                    docRun.AppendChild(new Text { Text = run.Item1, Space = SpaceProcessingModeValues.Preserve });
+                    docParagraph.AppendChild(docRun);
+                }
+                return docParagraph;
+            }
+            return null;
+        }
+        /// <summary>
+        /// Задание форматирования для абзаца
+        /// </summary>
+        /// <param name="paragraphProperties"></param>
+        /// <returns></returns>
+        private static ParagraphProperties CreateParagraphProperties(WordParagraphProperties paragraphProperties)
+        {
+            if (paragraphProperties != null)
+            {
+                ParagraphProperties properties = new ParagraphProperties();
+                properties.AppendChild(new Justification()
+                {
+                    Val = paragraphProperties.JustificationValues
+                });
+                properties.AppendChild(new SpacingBetweenLines
+                {
+                    LineRule = LineSpacingRuleValues.Auto
+                });
+                properties.AppendChild(new Indentation());
+                ParagraphMarkRunProperties paragraphMarkRunProperties = new ParagraphMarkRunProperties();
+                if (!string.IsNullOrEmpty(paragraphProperties.Size))
+                {
+                    paragraphMarkRunProperties.AppendChild(new FontSize { Val = paragraphProperties.Size });
+                }
+                properties.AppendChild(paragraphMarkRunProperties);
+                return properties;
+            }
+            return null;
+        }
+    }
+}
