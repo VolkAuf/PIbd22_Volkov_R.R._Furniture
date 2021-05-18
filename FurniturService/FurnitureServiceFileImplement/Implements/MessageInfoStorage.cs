8f21c6de7f293ba948aea8c34bd2d47d3b92b401
RAFMAN <volkov.rafman@mail.ru>
2021-05-18 17:57:23 +0400

    Lab7_fix

--- FurniturService/FurnitureServiceFileImplement/Implements/MessageInfoStorage.cs
+++ FurniturService/FurnitureServiceFileImplement/Implements/MessageInfoStorage.cs
@@ -4,6 +4,7 @@ using FurnitureServiceBusinessLogic.ViewModels;
 using FurnitureServiceFileImplement.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace FurnitureServiceFileImplement.Implements
